7456f71fb167456291f18aa2198ad154388cbec3
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 01:40:09 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,7 +20,7 @@ def remove_strawberry(contacts)
       if attribute == :favorite_ice_cream_flavors
         puts value
         if value.include?("strawberry")
-          puts value.pop(value.index("strawberry"))
+          value.pop(value.index("strawberry"))
         end
         puts value
       end
