179508de4b1a16b0aabd7b89ea2719e756dce1a8
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 02:06:20 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,16 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
+  contacts.each do |key, contact|
+    contact.collect do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        if value.include?("strawberry")
+          value.delete_at(value.index("strawberry"))
+        end
+      end
+    end
+  end
 
 end
